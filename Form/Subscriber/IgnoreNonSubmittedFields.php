ea5785bae094e92ae159026eee113b2bbf164537
Krzysztof Januś <fluke.kuczwa@gmail.com>
2014-10-14 11:21:42 +0200

    IgnoreNonSubmitedForms

---
+++ Form/Subscriber/IgnoreNonSubmittedFields.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tutto\Bundle\UtilBundle\Form\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+/**
+ * Class IgnoreNonSubmittedFields
+ * @package Tutto\Bundle\UtilBundle\Form\Subscriber
+ */
+class IgnoreNonSubmittedFields implements EventSubscriberInterface {
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents() {
+        return [FormEvents::PRE_SUBMIT => 'preSubmit'];
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function preSubmit(FormEvent $event) {
+        $form = $event->getForm();
+        $data = $event->getData();
+        foreach ($form->all() as $name => $child) {
+            if (!isset($data[$name])) {
+                $form->remove($name);
+            }
+        }
+
+    }
+}

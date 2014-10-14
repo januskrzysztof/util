6337567b5d409541ba4b07e49bcee6861a9f76c9
Krzysztof Januś <fluke.kuczwa@gmail.com>
2014-10-14 13:52:57 +0200

    PropertyAccessor

---
+++ Logic/PropertyAccessor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tutto\Bundle\UtilBundle\Logic;
+
+use Symfony\Component\PropertyAccess\PropertyAccessor as BasePropertyAccessor;
+
+/**
+ * Class PropertyAccessor
+ * @package Tutto\CommonBundle\PropertyAccess
+ */
+class PropertyAccessor extends BasePropertyAccessor {
+    /**
+     * @param array|object $objectOrArray
+     * @param string|\Symfony\Component\PropertyAccess\PropertyPathInterface $propertyPath
+     * @return mixed
+     */
+    public function getValue($objectOrArray, $propertyPath) {
+        if (is_array($objectOrArray)) {
+            $paths = explode('.', $propertyPath);
+            $propertyPath = '';
+            foreach ($paths as $path) {
+                $propertyPath.= '['.trim($path, '[]').']';
+            }
+        }
+
+        return parent::getValue($objectOrArray, $propertyPath);
+    }
+}
